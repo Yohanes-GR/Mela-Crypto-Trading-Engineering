74b2ca7dd101bd53cf05dc3613b272f96b01e389
degagawolde <degagawolde@gmail.com>
2022-10-15 17:57:51 +0000

    edited db.config file

--- backend/src/configs/db.config.js
+++ backend/src/configs/db.config.js
@@ -1,21 +1,23 @@
 require('dotenv').config();
-//const { Sequelize } = require('sequelize');
-//const USERNAME = process.env.POSTGRES_USER_NAME
-//const PSWD = process.env.POSTGRES_PASSWORD
-//const DBNAME = process.env.POSTGRES_DATABASE_NAME
-//const DBPORT = process.env.POSTGRES_PORT
+const { Sequelize } = require('sequelize');
+const USERNAME = process.env.POSTGRES_USER_NAME
+const PSWD = process.env.POSTGRES_PASSWORD
+const DBNAME = process.env.POSTGRES_DATABASE_NAME
+const SCHEMA_NAME = process.env.SECRET_SCHEMA
+const DBPORT = process.env.POSTGRES_PORT
 
-//const sequelize = new Sequelize(`postgres://${USERNAME}:${PSWD}@melachallengedatabase.crlafpfc5g5y.us-east-1.rds.amazonaws.com:${DBPORT}/${DBNAME}`);
-//module.exports = sequelize;
+
+const sequelize = new Sequelize(`postgres://${USERNAME}:${PSWD}@melachallengedatabase.crlafpfc5g5y.us-east-1.rds.amazonaws.com:${DBPORT}/${DBNAME}`);
+module.exports = sequelize;
 
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'huuser',
+  user: USERNAME,
   host: 'melachallengedatabase.crlafpfc5g5y.us-east-1.rds.amazonaws.com',
-  database: 'hudb',
-  schema:'public',
-  password: 'hupass',
-  port: 5432,
+  database: DBNAME,
+  schema:SCHEMA_NAME,
+  password: PSWD,
+  port: DBPORT
 })
 
-module.exports = pool
+module.exports = pool,sequelize
